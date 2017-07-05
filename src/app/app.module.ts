f76dc585da6f6f733e1ae04e5977dd65eeefe9e5
SkyDancer97 <skydance97@gmail.com>
2017-07-05 11:32:41 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ButtonModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
+import { TableViewComponent } from './component/table-view/table-view.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableViewComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
